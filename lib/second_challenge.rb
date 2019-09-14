acde66c44342ab269bd7919042345274905a0911
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-14 01:25:18 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,12 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
+    
+      :montague => {
       #your key/value pairs here!
-      :patriarch => {
+        :patriarch => {
         :name =>"Lord Montague"
-        :age => "53"},
+        :age => "53"
+        },
       :matriarch => {
         :name =>"Lady Montague"
         :age => "54"
