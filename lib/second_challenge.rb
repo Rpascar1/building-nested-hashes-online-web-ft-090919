b5adc874803065d6d4fa2866db635c4e2631e3c9
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-14 02:27:56 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -15,6 +15,7 @@ def second_challenge
         :status => "alive"
       },
       :hero_friends => [{:name => "Benvolio" , :age =>"17" ,:attitude =>"worried" }, {:name =>"Mercutio" , :age =>"18", :attitude =>"hot-headed" }]
+     
     }, 
       :capulet => {
       #your key/value pairs here!
